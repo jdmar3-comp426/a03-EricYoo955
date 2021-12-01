12916b16823947fb1f5bd84ba2854480165ece0c
EricYoo955 <ericyoo@ad.unc.edu>
2021-12-01 04:48:56 -0500

    removeKeys

--- test.js
+++ test.js
@@ -9,11 +9,16 @@
 // console.log(identifyVariable(4));
 
 // Testing identifyArray
-import { identifyArray } from "./src/mild/mild_2.js";
-console.log("Testing identifyArray(['some', 3, [3, 4], false]);");
-console.log(identifyArray(['some', 3, [3, 4], false]));
+// import { identifyArray } from "./src/mild/mild_2.js";
+// console.log("Testing identifyArray(['some', 3, [3, 4], false]);");
+// console.log(identifyArray(['some', 3, [3, 4], false]));
 
 // Testing removeKeyNonDestructive
 import { removeKeyNonDestructive } from "./src/mild/mild_2.js";
 console.log("Testing removeKeyNonDestructive");
 console.log(removeKeyNonDestructive({name: 'Mr. Boss',title: 'boss',age: 33,password: 'pass123'}, "password"));
+
+// Testing removeKeys
+import { removeKeys } from "./src/mild/mild_2.js";
+console.log("Testing removeKets");
+console.log(removeKeys({name: 'Mr. Boss',title: 'boss',age: 33,password: 'pass123'},['password', 'age']));
