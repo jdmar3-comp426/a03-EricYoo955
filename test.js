5d4cd9ba0e33ea28b807450433fe16d64b84bc90
EricYoo955 <ericyoo@ad.unc.edu>
2021-12-01 04:07:26 -0500

    identifyArray

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 // Testing sumToString
-import { sumToString } from "./src/mild/mild_1.js";
-console.log("Testing sumToString(3,4): ");
-console.log(sumToString(3,4));
+// import { sumToString } from "./src/mild/mild_1.js";
+// onsole.log("Testing sumToString(3,4): ");
+// console.log(sumToString(3,4));
 
+// Testing identifyVariable
 import { identifyVariable } from "./src/mild/mild_2.js";
 console.log("Testing identifyVariable(4)");
 console.log(identifyVariable(4));
+
+// Testing identifyArray
+import { identifyArray } from "./src/mild/mild_2.js";
+console.log("Testing identifyArray(['some', 3, [3, 4], false]);");
+console.log(identifyArray(['some', 3, [3, 4], false]));
